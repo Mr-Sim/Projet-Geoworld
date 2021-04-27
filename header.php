04cf20cb3886e4abd38beec6d17f976b127cf125
GlobeTique77 <78152264+GlobeTique77@users.noreply.github.com>
2021-04-27 22:22:25 +0200

    test

--- header.php
+++ header.php
@@ -55,9 +55,6 @@ session_start();
         <li class="nav-item active">
           <a class="nav-link" href="index.php">Home <span class="sr-only">(current)</span></a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Link</a>
-        </li>
         <li class="nav-item dropdown">
           <a class="nav-link dropdown-toggle" href="#" id="dropdown01" data-toggle="dropdown" aria-haspopup="true"
              aria-expanded="false">Continents</a>
@@ -73,20 +70,15 @@ session_start();
       <ul class="navbar-nav ml-auto">
         <li class="nav-item">
           <?php if (isset($_SESSION['nom'])):?>
-          <a class="nav-link" href="logout.php">Logout</a>
+          <a class="nav-link" href="logout.php">Déconnexion</a>
           <?php else: ?>
-          <a class="nav-link" href="authentification.php">Login</a>
+          <a class="nav-link" href="authentification.php">Connexion</a>
           <?php endif; ?>
         </li>
         <li class="nav-item">
           <?php if (isset($_SESSION['role']) && $_SESSION['role']=="admin"): ?>
           <a class="nav-link" href="users.php">Espace admin</a>
           <?php endif; ?>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link " href="todo-projet.php">
-            Présentation-Atelier-de-Prof-SLAM
-          </a>
         </li>
       </ul>
     </div>
